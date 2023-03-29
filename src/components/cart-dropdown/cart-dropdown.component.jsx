4c925ec4df98c608fe7bc075a9fa9e31bca8ee8b
sabanansari <alisaban52@gmail.com>
2023-03-29 08:56:17 +0530

    created checkout page,checkout items and checkout cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import Button from '../button/button.component';
 import './cart-dropdown.styles.scss';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 import CartItem from '../cart-item/cart-item.component';
 
@@ -9,6 +10,12 @@ const CartDropdown = ()=>{
 
     const {cartItems} = useContext(CartContext);
 
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = ()=>{
+        navigate('/checkout');
+    }
+
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -16,7 +23,7 @@ const CartDropdown = ()=>{
                     <CartItem key={item.id} cartItem={item} />
                 ))}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick = {goToCheckoutHandler}>GO TO CHECKOUT</Button>
         </div>
     )
 }
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,41 @@
+import './checkout.styles.scss';
+import { useContext } from 'react'; 
+import { CartContext } from '../../contexts/cart.context';
+import CheckOutItem from '../../components/checkout-item/checkout-item.component';
+
+const CheckOut = ()=>{
+
+    const {cartItems,addItemToCart,removeItemFromCart,cartTotal} = useContext(CartContext);
+
+    return(
+        <div className='checkout-container'>
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span>Product</span>
+            </div>
+            <div className='header-block'>
+            <span>Description</span>
+            </div>
+            <div className='header-block'>
+            <span>Quantity</span>
+            </div>
+            <div className='header-block'>
+            <span>Price</span>
+            </div>
+            <div className='header-block'>
+            <span>Remove</span>
+            </div>
+        </div>
+                {cartItems.map((cartItem)=>{
+                    const {id,name,quantity} = cartItem;
+                    return(
+                        <CheckOutItem  key={cartItem.id} cartItem={cartItem}/>
+                    )
+                })}
+                <span className='total'>{cartTotal}$</span>
+        </div>
+        
+    )
+}
+
+export default CheckOut;
