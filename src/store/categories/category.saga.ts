a976a7862d4975c243023102af5378630556f50c
sabanansari <alisaban52@gmail.com>
2023-04-11 02:10:15 +0530

    migrated sagas from js to ts

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,14 +1,14 @@
-import { takeLatest,all,call,put } from "redux-saga/effects";
+import { takeLatest,all,call,put } from "typed-redux-saga/macro";
 import { getCategoriesAndDOcuments } from "../../utils/firebase/firebase.utils";
 import { fetchCategoriesSuccess,fetchCategoriesFailed } from "./category.action";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 export function* fetchCategoriesAsync(){
     try{
-        const categoriesArray = yield call(getCategoriesAndDOcuments, 'categories');
+        const categoriesArray = yield* call(getCategoriesAndDOcuments);
         yield put(fetchCategoriesSuccess(categoriesArray));
     }catch(error){
-       yield put(fetchCategoriesFailed(error));
+       yield put(fetchCategoriesFailed(error as Error));
     }
 }
 
--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -1,6 +1,7 @@
 import { createAction,withMatcher,Action,ActionWithPayload } from "../../utils/reducer/reducer.util";
 import { USER_ACTION_TYPES } from "./user.types";
 import { UserData, AdditionalInformation } from "../../utils/firebase/firebase.utils";
+import { User } from "firebase/auth";
 
 export type CheckUserSession = Action<USER_ACTION_TYPES.CHECK_USER_SESSION>;
 
@@ -16,7 +17,7 @@ export type SignInFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_IN_FAILED,Er
 
 export type SignUpStart = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_START,{email:string,password:string,displayName:string;}>;
 
-export type SignUpSuccess = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_SUCCESS,{user:UserData,additionalDetails:AdditionalInformation}>;
+export type SignUpSuccess = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_SUCCESS,{user:User,additionalDetails:AdditionalInformation}>;
 
 export type SignUpFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_FAILED,Error>;
 
@@ -34,13 +35,13 @@ export const googleSignInStart = withMatcher(():GoogleSignInStart=> createAction
 
 export const emailSignInStart = withMatcher((email:string,password:string):EmailSignInStart=> createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START,{email,password}));
 
-export const signInSuccess = withMatcher((user:UserData):SignInSuccess=> createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS,user));
+export const signInSuccess = withMatcher((user:UserData & {id:string}):SignInSuccess=> createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS,user));
 
 export const signInFailed = withMatcher((error:Error):SignInFailed=> createAction(USER_ACTION_TYPES.SIGN_IN_FAILED,error));
 
 export const signUpStart = withMatcher((email:string,password:string,displayName:string):SignUpStart => createAction(USER_ACTION_TYPES.SIGN_UP_START,{email,password,displayName}));
 
-export const signUpSuccess = withMatcher((user:UserData,additionalDetails:AdditionalInformation):SignUpSuccess=>createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS,{user,additionalDetails}));
+export const signUpSuccess = withMatcher((user:User,additionalDetails:AdditionalInformation):SignUpSuccess=>createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS,{user,additionalDetails}));
 
 export const signUpFailed = withMatcher((error:Error):SignUpFailed=>createAction(USER_ACTION_TYPES.SIGN_UP_FAILED,error));
 
